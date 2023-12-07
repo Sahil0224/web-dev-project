e10834d2f2eb522cae8c89207856cd06ffc8e207
Patel <sahil.patel@fiserv.com>
2023-12-07 16:02:58 -0500

    Displaying menu.

--- web-dev-crash-course-project/js/menu.js
+++ web-dev-crash-course-project/js/menu.js
@@ -3,27 +3,81 @@ class Menu {
 
   constructor(container) {
     this.container = container;
+    this.renderMenu();
     this.render();
   }
 
-  /**
-   * Create an item component and append it as a child to the container
-   */
-  #renderItem(container, item) {
-    // TODO: Render the item name, description, price, and add to cart button
+  async renderMenu() {
+    const data = await fetch('https://dev1.dev.clover.com/oloservice/v1/merchants/R9AHC6Q4K7PX1/menu', {
+      method: 'GET',
+      mode: 'cors',
+    }).then((res) => res.json());
+
+    this.displayMenuItemsByCategory(data);
   }
 
-  /**
-   * Create a category component and append it as a child to the container
-   */
-  #renderCategory(container, category) {
-    // TODO: Render the category name
+  displayMenuItemsByCategory(menu) {
+    const menuContent = document.getElementById('menu-content');
+
+    menuContent.innerHTML = '';
+
+    for (const categoryId in menu.categories) {
+      const category = menu.categories[categoryId];
+
+      const categoryContainer = document.createElement('div');
+      categoryContainer.classList.add('category-container');
+
+      const categoryHeader = document.createElement('h2');
+      categoryHeader.textContent = category.name;
+      categoryContainer.appendChild(categoryHeader);
+
+      const itemsContainer = document.createElement('div');
+      itemsContainer.classList.add('items-container');
+
+      let itemsInRow = 0; 
+      category.items.forEach((itemId) => {
+        const item = menu.items.find((item) => item.id === itemId);
+        if (item) {
+          const itemDetails = document.createElement('div');
+          itemDetails.classList.add('item-details');
+
+          const itemName = document.createElement('p');
+          itemName.textContent = item.name;
+
+          const itemPrice = document.createElement('p');
+          itemPrice.textContent = `$${(item.price / 100).toFixed(2)}`;
+
+          const itemImage = document.createElement('img');
+          itemImage.alt = item.name;
+          itemImage.src = item.images[0]?.source;
+
+          itemDetails.appendChild(itemName);
+          itemDetails.appendChild(itemPrice);
+          itemDetails.appendChild(itemImage);
+
+          itemsContainer.appendChild(itemDetails);
+
+          itemsInRow++;
+          if (itemsInRow >= 2) {
+            itemsInRow = 0;
+          }
+        }
+      });
+
+      categoryContainer.appendChild(itemsContainer);
+
+      menuContent.appendChild(categoryContainer);
+    }
   }
 
   render() {
-    this.container.innerHTML = `<div>!! --Menu Component-- !!</div>`;
-
-    // TODO: Render each category
-      // TODO: Render each item in the category
+    this.container.innerHTML = `
+      <main>
+        <h1>Menu</h1>
+        <section id="menu-content"></section>
+      </main>
+    `;
   }
 }
+
+const menu = new Menu(document.body);
--- web-dev-crash-course-project/js/merchant-hero.js
+++ web-dev-crash-course-project/js/merchant-hero.js
@@ -1,42 +1,98 @@
 class MerchantHero {
   container;
-
+ 
   constructor(container) {
-    this.container = container;
-    this.render();
-    this.populateMerchantHero();
+      this.container = container;
+      this.render();
+      this.populateMerchantData();
   }
-
-  async populateMerchantHero() {
-     const data = await fetch('https://dev1.dev.clover.com/oloservice/v1/merchants/R9AHC6Q4K7PX1', {
-      method: 'GET',
-      mode: 'cors'
-     }).then(res => res.json());
-
-     const merchantLogo = data.logo;
-     const merchantName = data.name;
-     const merchantAddress = data.address.address1;
-     const merchantAddress2 = data.address.address2;
-     const merchantCity = data.address.city;
-     const merchantState = data.address.state;
-     const merchantPhone = data.phone;
-     const address = `${merchantAddress}, ${merchantAddress2}, ${merchantCity} ${merchantState}`
-     this.container.innerHTML = `
-    <div class="merchant-wrapper">
-        <div class="merchant-info">
-            <div class="merchant-image">
-                <img src="${merchantLogo}" alt="${merchantName} Image">
-            </div>
-            <div class="merchant-details">
-                <div class="merchant-name">${merchantName}</div>
-                <div class="merchant-address">${address}</div>
-                <div class="merchant-phone">${merchantPhone}</div>
-            </div>
-        </div>
-    </div>`;
+ 
+  async populateMerchantData() {
+      try {
+          this.container.innerHTML += '<div>Loading Merchant Data...</div>';
+ 
+          const data = await fetch('https://dev1.dev.clover.com/oloservice/v1/merchants/R9AHC6Q4K7PX1', {
+              method: 'GET',
+              mode: 'cors'
+          }).then(res => res.json());
+ 
+          const merchantImage = data.logo;
+          const merchantName = data.name;
+ 
+          const address1 = data.address.address1;
+          const address2 = data.address.address2;
+          const city = data.address.city;
+          const state = data.address.state;
+ 
+          const merchantNumber = data.phone;
+          const operatingHours = this.getDineInHours(data);
+          const fullAddress = `${address1} ${address2}, ${city}, ${state}`;
+ 
+          this.container.innerHTML = `
+              <div class="merchant-wrapper">
+                  <div class="merchant-info">
+                      <div class="merchant-image">
+                          <img src="${merchantImage}" alt="${merchantName} Image">
+                      </div>
+                      <div class="merchant-details">
+                          <div class="merchant-name">${merchantName}</div>
+                          <div class="merchant-address">${fullAddress}</div>
+                          <div class="merchant-phone">${merchantNumber}</div>
+                      </div>
+                      <div class="operating-hours"><strong> Open</strong> ${operatingHours}</div>
+                  </div>
+              </div>
+          `;
+      } catch (e) {
+          console.log('Something went wrong :(', e);
+      }
   }
-
+ 
   render() {
-    this.container.innerHTML = `<div>!! --Merchant Hero Component-- !!</div>`;
+      this.container.innerHTML = `<div class="merchant-info">!! --Merchant Hero Component-- !!</div>`;
+  }
+ 
+  getCurrentDay() {
+      const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+      const currentDate = new Date();
+      const currentDayIndex = currentDate.getDay(); 
+      return days[currentDayIndex];
+  }
+  
+  convertToRegularTime(militaryTime) {
+    if (militaryTime.length === 3) {
+      militaryTime = '0' + militaryTime;
+  }
+ 
+  const hours = parseInt(militaryTime.substring(0, 2));
+  const minutes = parseInt(militaryTime.substring(2));
+ 
+  let period = 'AM';
+  let regularHours = hours;
+ 
+  if (hours >= 12) {
+      period = 'PM';
+      regularHours = (hours === 12) ? 12 : hours - 12;
+  }
+ 
+  if (regularHours === 0) {
+      regularHours = 12;
+  }
+ 
+  const regularMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+ 
+  return `${regularHours}:${regularMinutes} ${period}`;
+}
+ 
+  getDineInHours(data) {
+      const currentDay = this.getCurrentDay().toLowerCase();
+      const dineInService = data.services.find(service => service.type === "DINE_IN");
+ 
+      if (dineInService && dineInService.hours && dineInService.hours[currentDay]) {
+          const dineInHours = dineInService.hours[currentDay];
+          return dineInHours.map(slot => `${this.convertToRegularTime(slot.start)} - ${this.convertToRegularTime(slot.end)}`);
+      } else {
+          return ['Closed'];
+      }
   }
 }
